b114e6d7b12f97a01d484b60ac6b64d2e9cc8ebb
afifi brahim <afifi71brahim@gmail.com>
2024-10-25 23:18:37 +0100

    file and folder instruction

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,47 @@
 import React from "react";
-
+import { CssBaseline } from "@mui/material";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+// import { ThemeProvider, createTheme } from "@mui/material/styles";
+import useStyles from "./styles";
+// import components
+import {
+  Actors,
+  MovieInformation,
+  Movies,
+  NavBar,
+  Profile,
+} from "./components";
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Movies />,
+  },
+  {
+    path: "/movie/:id",
+    element: <MovieInformation />,
+  },
+  {
+    path: "/actors/:id",
+    element: <Actors />,
+  },
+  {
+    path: "/profile/:id",
+    element: <Profile />,
+  },
+]);
 const App = () => {
-  return <div>App</div>;
+  const classes = useStyles();
+  return (
+    <div>
+      <CssBaseline />
+      <NavBar />
+      <main className={classes.content}>
+        <div className={classes.toolbar} />
+        <RouterProvider router={router} />
+      </main>
+      {/* <div ref={alanBtnContainer} /> */}
+    </div>
+  );
 };
 
 export default App;
