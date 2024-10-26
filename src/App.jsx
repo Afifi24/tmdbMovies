a90efe197ea10b6b45a7fbc537884f6c2a4ebf84
afifi brahim <afifi71brahim@gmail.com>
2024-10-26 13:40:21 +0100

    add navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { CssBaseline } from "@mui/material";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-// import { ThemeProvider, createTheme } from "@mui/material/styles";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
 import useStyles from "./styles";
 // import components
 import {
@@ -14,33 +14,39 @@ import {
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Movies />,
-  },
-  {
-    path: "/movie/:id",
-    element: <MovieInformation />,
-  },
-  {
-    path: "/actors/:id",
-    element: <Actors />,
-  },
-  {
-    path: "/profile/:id",
-    element: <Profile />,
+    element: <NavBar />,
+    children: [
+      {
+        path: "/",
+        element: <Movies />,
+      },
+      {
+        path: "/movie/:id",
+        element: <MovieInformation />,
+      },
+      {
+        path: "/actors/:id",
+        element: <Actors />,
+      },
+      {
+        path: "/profile/:id",
+        element: <Profile />,
+      },
+    ],
   },
 ]);
 const App = () => {
   const classes = useStyles();
+  const theme = createTheme();
   return (
-    <div>
+    <ThemeProvider theme={theme}>
       <CssBaseline />
-      <NavBar />
       <main className={classes.content}>
         <div className={classes.toolbar} />
         <RouterProvider router={router} />
       </main>
       {/* <div ref={alanBtnContainer} /> */}
-    </div>
+    </ThemeProvider>
   );
 };
 
